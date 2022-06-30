0968a5a170c21d8bccd3a0e970293c9d313afeaa
cherediamultimedia <98904939+cherediamultimedia@users.noreply.github.com>
2022-06-30 09:52:08 +0200

    Commit on TextView

---
+++ 11.Android-TextView-LinearLayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.welcome.tutoria_2_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
